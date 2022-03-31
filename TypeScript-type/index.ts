ac13b1a39a410602971cf45f9a0ff480f3e5199f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-01 05:40:53 +0900

    「型注釈」と「型推論」

--- TypeScript-type/index.ts
+++ TypeScript-type/index.ts
@@ -1,3 +1,4 @@
+// 型注釈（型を明示的に宣言）
 let hasValue: boolean = true;
 let count: number = 10;
 let float: number = 3.14; // 小数OK
@@ -5,3 +6,12 @@ let negative: number = -0.12; // マイナス値OK
 let single: string = "hello";
 let double: string = "hello";
 let back: string = `hello`;
+
+// 型推論（代入値から型を推論）
+let hasValue2 = true;
+let count2 = 10;
+let float2 = 3.14; // 小数OK
+let negative2 = -0.12; // マイナス値OK
+let single2 = "hello";
+let double2 = "hello";
+let back2 = `hello`;
