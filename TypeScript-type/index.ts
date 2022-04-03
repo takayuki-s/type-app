63406326cc6778b427e31aca35b6e2cf457c483c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-03 09:17:34 +0900

    Literal型
    constはLiteral型

--- TypeScript-type/index.ts
+++ TypeScript-type/index.ts
@@ -79,3 +79,18 @@ let unionTypes: (number | string)[] = [21, "hello"];
 // unionType.toUpperCase() // 数値型の場合はエラー
 unionType = "hello";
 unionType.toUpperCase(); // 文字列型になったのでエラーがなくなる
+
+// Literal型
+const apple: "apple" = "apple"; // "apple"で設定すると、"apple"以外はエラーとなる
+const orange = "orange"; // 型注釈を当てなくても、型推論で"orange"となる
+
+// Union型とLiteral型の複合でenum型のように使える（普通オブジェクトには適用できない）
+let clothSize: "small" | "medium" | "large" = "small";
+// オブジェクトに適用したい場合
+const cloth: {
+  color: string;
+  size: "small" | "medium" | "large";
+} = {
+  color: "white",
+  size: "medium",
+};
