a2c63f1b52bc36c34f9c72d873a5348845fe01fc
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-06 07:00:56 +0900

    getterとsetter
    getterとsetterを同じ名前で定義した場合、getterの型注釈がsetterにもそのまま適用される

--- TypeScript-class/dist/class.js
+++ TypeScript-class/dist/class.js
@@ -13,9 +13,21 @@ class Person {
     }
 }
 class Teacher extends Person {
-    constructor(name, age, subject) {
+    constructor(name, age, _subject) {
         super(name, age);
-        this.subject = subject;
+        this._subject = _subject;
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error("There is no subject");
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error("There is no subject");
+        }
+        this._subject = value;
     }
     greeting() {
         console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`);
@@ -26,4 +38,7 @@ const quill = new Person("quill", 38);
 quill.incrementAge();
 quill.greeting();
 const teacher = new Teacher("Teke", 33, "Math");
+console.log(teacher.subject);
+teacher.subject = "Music";
+console.log(teacher.subject);
 teacher.greeting();
--- TypeScript-class/src/class.ts
+++ TypeScript-class/src/class.ts
@@ -11,7 +11,19 @@ class Person {
 }
 
 class Teacher extends Person {
-  constructor(name: string, age: number, public subject: string) {
+  get subject(): string {
+    if (!this._subject) {
+      throw new Error("There is no subject");
+    }
+    return this._subject;
+  }
+  set subject(value) {
+    if (!value) {
+      throw new Error("There is no subject");
+    }
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
     super(name, age);
   }
   greeting(this: Teacher) {
@@ -27,4 +39,7 @@ quill.incrementAge();
 quill.greeting();
 
 const teacher = new Teacher("Teke", 33, "Math");
+console.log(teacher.subject);
+teacher.subject = "Music";
+console.log(teacher.subject);
 teacher.greeting();
