19420d0c981df2daa2ec290a6ce52f966da94e1d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-08 06:35:03 +0900

    メソッドをオブジェクトの型に指定

--- TypeScript-interface/dist/interface.js
+++ TypeScript-interface/dist/interface.js
@@ -1,1 +1,8 @@
 "use strict";
+const human = {
+    name: "Quill",
+    age: 38,
+    greeting(message) {
+        console.log(message);
+    },
+};
--- TypeScript-interface/src/interface.ts
+++ TypeScript-interface/src/interface.ts
@@ -1,11 +1,13 @@
 interface Human {
   name: string;
   age: number;
+  greeting(message: string): void;
 }
 
-const human: Human = {
+const human = {
   name: "Quill",
   age: 38,
+  greeting(message: string): void {
+    console.log(message);
+  },
 };
-
-let developer: Human;
