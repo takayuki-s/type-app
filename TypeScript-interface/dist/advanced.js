4f2f2d6d5ad1af462e3786145dd8c91d485e0a99
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-09 07:13:29 +0900

    条件文を使って型を絞り込む、３つのType guard

---
+++ TypeScript-interface/dist/advanced.js
@@ -0,0 +1,41 @@
+"use strict";
+const quill = {
+    name: "Quill",
+    role: "front-end",
+    follower: 1000,
+};
+function toUpperCase(x) {
+    if (typeof x === "string") {
+        return x.toUpperCase();
+    }
+    return "";
+}
+function describeProfile(nomadWorker) {
+    console.log(nomadWorker.name);
+    if ("role" in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ("follower" in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+class Dog {
+    speak() {
+        console.log("bow-wow");
+    }
+}
+class Bird {
+    speak() {
+        console.log("tweet-tweet");
+    }
+    fly() {
+        console.log("flutter");
+    }
+}
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());
