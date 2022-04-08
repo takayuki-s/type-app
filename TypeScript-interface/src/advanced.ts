adcf5f8b485b910b7ec1964942c08c5fa29e4118
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-09 06:46:34 +0900

    &を用いてインターセクション型を定義

---
+++ TypeScript-interface/src/advanced.ts
@@ -0,0 +1,22 @@
+interface Engineer {
+  name: string;
+  role: string;
+}
+
+interface Blogger {
+  name: string;
+  follower: number;
+}
+
+// type EngineerBlogger = Engineer & Blogger;
+interface EngineerBlogger extends Engineer, Blogger {}
+
+const quill: EngineerBlogger = {
+  name: "Quill",
+  role: "front-end",
+  follower: 1000,
+};
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber;
