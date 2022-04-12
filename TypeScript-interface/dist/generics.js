d43560b6e736bd3180d79d40a924330ca1c6495e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-13 08:10:03 +0900

    keyofを使ってオブジェクトのキーのユニオン型を作成

--- TypeScript-interface/dist/generics.js
+++ TypeScript-interface/dist/generics.js
@@ -8,3 +8,8 @@ function extendsCopy(value) {
 console.log(copy("hello"));
 console.log(copy({ name: "Quill" })); // 型推論
 console.log(extendsCopy({ name: "Quill" })); // {name: "文字列"}の形でないとエラーとなる
+function keyofCopy(value, key) {
+    value[key];
+    return value;
+}
+console.log(keyofCopy({ name: "Quill", age: 38 }, "name"));
--- TypeScript-interface/src/generics.ts
+++ TypeScript-interface/src/generics.ts
@@ -7,3 +7,14 @@ function extendsCopy<T extends { name: string }>(value: T): T {
 console.log(copy<string>("hello"));
 console.log(copy({ name: "Quill" })); // 型推論
 console.log(extendsCopy({ name: "Quill" })); // {name: "文字列"}の形でないとエラーとなる
+
+// keyof
+type K = keyof { name: string; age: number }; // 基本的な記述方法
+function keyofCopy<T extends { name: string }, U extends keyof T>(
+  value: T,
+  key: U
+): T {
+  value[key];
+  return value;
+}
+console.log(keyofCopy({ name: "Quill", age: 38 }, "name"));
