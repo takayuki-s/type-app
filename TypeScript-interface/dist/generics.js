0627aaad2e8de99d7a273c8e8e1e4490dcedaedc
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-13 08:00:29 +0900

    型パラメータに制約（extends）

--- TypeScript-interface/dist/generics.js
+++ TypeScript-interface/dist/generics.js
@@ -2,5 +2,9 @@
 function copy(value) {
     return value;
 }
+function extendsCopy(value) {
+    return value;
+}
 console.log(copy("hello"));
 console.log(copy({ name: "Quill" })); // 型推論
+console.log(extendsCopy({ name: "Quill" })); // {name: "文字列"}の形でないとエラーとなる
--- TypeScript-interface/src/generics.ts
+++ TypeScript-interface/src/generics.ts
@@ -1,5 +1,9 @@
 function copy<T>(value: T): T {
   return value;
 }
+function extendsCopy<T extends { name: string }>(value: T): T {
+  return value;
+}
 console.log(copy<string>("hello"));
 console.log(copy({ name: "Quill" })); // 型推論
+console.log(extendsCopy({ name: "Quill" })); // {name: "文字列"}の形でないとエラーとなる
