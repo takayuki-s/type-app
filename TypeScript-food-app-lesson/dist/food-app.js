1f5da09db68ca02796e975950d6c9fbdd8db7a90
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-20 08:42:43 +0900

    FoodクラスとFoodsクラスの役割分け

--- TypeScript-food-app-lesson/dist/food-app.js
+++ TypeScript-food-app-lesson/dist/food-app.js
@@ -2,6 +2,16 @@
 class Score {
 }
 class Food {
+    constructor(element) {
+        this.element = element;
+    }
 }
 class Foods {
+    constructor() {
+        this.elements = document.querySelectorAll(".food");
+        this.elements.forEach((element) => {
+            new Food(element);
+        });
+    }
 }
+const foods = new Foods();
--- TypeScript-food-app-lesson/src/food-app.ts
+++ TypeScript-food-app-lesson/src/food-app.ts
@@ -1,3 +1,13 @@
 class Score {}
-class Food {}
-class Foods {}
+class Food {
+  constructor(public element: HTMLDivElement) {}
+}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>(".food");
+  constructor() {
+    this.elements.forEach((element) => {
+      new Food(element);
+    });
+  }
+}
+const foods = new Foods();
