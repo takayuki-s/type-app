59858a4e084aef83e55f4c7716b1bc02a3faf297
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-20 08:51:27 +0900

    Foodクラスにクリックイベント
    thisをbind

--- TypeScript-food-app-lesson/src/food-app.ts
+++ TypeScript-food-app-lesson/src/food-app.ts
@@ -1,6 +1,11 @@
 class Score {}
 class Food {
-  constructor(public element: HTMLDivElement) {}
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener("click", this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    this.element.classList.toggle("food--active");
+  }
 }
 class Foods {
   elements = document.querySelectorAll<HTMLDivElement>(".food");
