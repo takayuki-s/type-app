1f644fb2a1ffb21955e9405f8fe5482d19b11763
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-24 16:40:10 +0900

    tsloader設定、tsファイルからバンドル

--- TypeScript-food-app-lesson/webpack.config.js
+++ TypeScript-food-app-lesson/webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 module.exports = {
-  entry: "./dist/main.js",
+  entry: "./src/main.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
