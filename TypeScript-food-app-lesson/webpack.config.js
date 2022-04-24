5594dda0af63315c9b6d81d6969e242292a5c5cc
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-25 07:37:41 +0900

    webpack-dev-serverの使用
    バージョンによるエラーを解決

--- TypeScript-food-app-lesson/webpack.config.js
+++ TypeScript-food-app-lesson/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 module.exports = {
   entry: "./src/main.ts",
+  mode: "development",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
