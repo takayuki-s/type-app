8b05859c1d395e1df0a6d5b6a3f163c6e0a3d30f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-26 07:11:58 +0900

    namespaceで型を定義

--- TypeScript-library/src/library.ts
+++ TypeScript-library/src/library.ts
@@ -4,5 +4,9 @@ console.log(_.shuffle([1, 2, 3, 4]));
 namespace myApp {
   const hello = "hello in namespace";
   export const name = "Quill";
+  export interface Nameable {
+    name: string;
+  }
 }
+let nameable: myApp.Nameable;
 const name = myApp.name;
