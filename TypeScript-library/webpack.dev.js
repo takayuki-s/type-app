acf8286aa9528bd429ab17a7aa037ed5946f985c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-04-26 06:09:37 +0900

    型定義ファイル作成準備

---
+++ TypeScript-library/webpack.dev.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/library.ts",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
---
+++ TypeScript-library/webpack.prod.js
@@ -0,0 +1,23 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/library.ts",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
