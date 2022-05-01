685d39b748765437448e1e38c63d0af6e81de52d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-02 07:36:04 +0900

    require文をimport文に

--- TypeScript-node/dist/app.js
+++ TypeScript-node/dist/app.js
@@ -1,3 +1,8 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// const express = require("express");
+const app = (0, express_1.default)();
--- TypeScript-node/src/app.ts
+++ TypeScript-node/src/app.ts
@@ -1,2 +1,3 @@
-const express = require("express");
+import express from "express";
+// const express = require("express");
 const app = express();
